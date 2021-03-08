dc954b55f2a15e07b63537ebad2704eed4746771
Will Pennell <will.phanta@gmail.com>
2021-03-08 20:47:51 +0000

    First Deployment

---
+++ README.md
@@ -0,0 +1,5 @@
+# ResearchKeywordInPaper
+
+Script takes keyword and file path, it will loop through all .pdf files in the folder and find sentences containing the keyword.
+
+it will add these to a folder with the same name as the keyword and create a text file for each pdf with the sentences located.
---
+++ Scripts/Main.py
@@ -0,0 +1,30 @@
+#! python3
+# keywordSentenceLocater.py - enter folder path location and keyword or search term, this will read all pdfs in and add all the sentences that contains keyword into a txt file.
+import re, os, pdfFormat
+from pathlib import Path
+#Inputs for file location and keyword
+keyword = input('Enter Keyword/Search term: ')
+location = Path(input('Enter FOLDER location: '))
+searchRegex = re.compile(f'[^.?!]*(?<=[.?\s!]){keyword}[a-zA-Z]*(?=[\s.?!])[^.?!]*[.?!]')
+
+os.chdir(location)
+keywordFolder = pdfFormat.folderCreator(location, keyword)
+print(keywordFolder)
+for foldername, subfolders, filenames in os.walk(location):
+    #Loop through each PDF in folder
+    for filename in filenames:
+        if filename.endswith('.pdf'):
+            print(filename)
+            rawText = pdfFormat.PDF2Text(filename)
+            sentences = re.findall(searchRegex, rawText)
+            if len(sentences) > 0:
+                newFileName = pdfFormat.fileRenamer(filename, keyword)
+                #Create new folder named findings
+                KeywordFileLocation = open(Path(keywordFolder / newFileName), 'w', encoding='utf-8')
+                KeywordFileLocation.write('---' + filename + '---\n\n\n\n')
+                for sentence in sentences:
+                    KeywordFileLocation.write(sentence + '\n')
+                KeywordFileLocation.close()
+ 
+            else:
+                print(f'No sentences containing {keyword} in {filename}.')
